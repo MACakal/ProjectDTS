c6126d11efe7a9fcec4101f392233283dac739f0
Basel-1 <159136975+Basel-1@users.noreply.github.com>
2026-04-07 10:21:20 +0200

    text update

--- Presentation/ViewProductPres.cs
+++ Presentation/ViewProductPres.cs
@@ -1,5 +1,7 @@
 namespace ProjectDTS;
 
+using System.Windows;
+
 public class ViewProductPres
 {
     private ProductService _productService;
@@ -30,21 +32,51 @@ public class ViewProductPres
 
     public void DisplayProducts(IEnumerable<Product> items)
     {
-        Console.Clear();
-        Console.WriteLine("\n---- Product List ----");
+        ConsoleColor[] colors = {
+    ConsoleColor.Red,
+    ConsoleColor.Yellow,
+    ConsoleColor.Green,
+    ConsoleColor.Cyan,
+    ConsoleColor.Blue,
+    ConsoleColor.Magenta
+    };
+
+        string text = "\n            --------- Product List ---------";
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            Console.ForegroundColor = colors[i % colors.Length];
+            Console.Write(text[i]);
+        }
+
+        Console.WriteLine();
+        Console.ResetColor();
 
         if (!items.Any())
         {
             Console.WriteLine("There are no products to show.");
             return;
         }
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"{"ID",-5} {"Name",-30} {"Category",-20} {"Price",10}");
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine(new string('-', 70));
 
-        Console.WriteLine($"{"ID",-5} {"Name",-20} {"Category",-20} {"Price",10}");
-        Console.WriteLine(new string('-', 60));
 
         foreach (var p in items)
         {
-            Console.WriteLine($"{p.Id,-5} {p.Name,-20} {p.Category,-20} {p.Price,10}€");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Console.Write($"{p.Id,-5}");  // { p.Name,-20} { p.Category,-20} { p.Price,10}€");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write($"{p.Name,-30}");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write($"{p.Category,-20}");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"€{p.Price,10}");
+            Console.ResetColor();
         }
     }
 }
